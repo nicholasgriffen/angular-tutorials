0c61480057a9b7c6cd62edb7eae86838f4b5e10f
nicholasgriffen <nicholas.s.griffen@gmail.com>
2018-12-03 13:49:32 -0700

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'heroes'`, () => {
+  it(`should have as title 'Tracking Heroes'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('heroes');
+    expect(app.title).toEqual('Tracking Heroes');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to heroes!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Tracking Heroes!');
   });
 });
