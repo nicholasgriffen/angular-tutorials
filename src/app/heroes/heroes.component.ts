bc8b606cba92a4ac04b44bef28352dd8a65a10cc
nicholasgriffen <nicholas.s.griffen@gmail.com>
2018-12-04 09:23:34 -0700

    create async hero fetching service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero'
-import { HEROES } from '../mock-heroes'
+import { HeroService } from '../hero.service'
 
 @Component({
   selector: 'app-heroes',
@@ -9,17 +9,22 @@ import { HEROES } from '../mock-heroes'
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  constructor(private heroService: HeroService) { }
 
-  heroes: Hero[] = HEROES
+  selectedHero: Hero  
   
-  selectedHero: Hero
   onSelect(hero: Hero): void {
     this.selectedHero = hero
   }
-
-  constructor() { }
-
-  ngOnInit() {
+  
+  heroes: Hero[]
+  
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes)
   }
 
+  ngOnInit() {
+    this.getHeroes()
+  }
 }
