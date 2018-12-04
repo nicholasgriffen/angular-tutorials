c2029066c54261095e247a532cb66c54d2cf84b9
nicholasgriffen <nicholas.s.griffen@gmail.com>
2018-12-04 08:11:51 -0700

    list heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero'
+import { HEROES } from '../mock-heroes'
 
 @Component({
   selector: 'app-heroes',
@@ -8,10 +9,8 @@ import { Hero } from '../hero'
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
+
+  heroes: Hero[] = HEROES
 
   constructor() { }
 
